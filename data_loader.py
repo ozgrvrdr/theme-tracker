f130ff9a270f9b0ab01854431c711eb3edf571ee
ozgrvrdr <ozgrvrdr@gmail.com>
2026-08-13 21:14:59 +0300

    Reduce cache duration from 30 minutes to 5 minutes

--- data_loader.py
+++ data_loader.py
@@ -2,7 +2,7 @@ import streamlit as st
 import yfinance as yf
 from config import BENCHMARK
 
-@st.cache_data(ttl=1800)  # Verileri 30 dakika hafızada tutar (Ön-bellek)
+@st.cache_data(ttl=300)  # Verileri 30 dakika yerine 5 dakikada bir yeniler
 def fetch_market_data(tickers):
     all_tickers = list(set(tickers + [BENCHMARK]))
     data = yf.download(all_tickers, period="2y", interval="1d", progress=False)["Close"]
